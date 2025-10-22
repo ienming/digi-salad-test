b63dc5d7c986dbb8a181f8b6989a8369d6dac1ee
ienming <hsieh.alan.0726@gmail.com>
2025-10-22 14:18:06 +0800

    init

---
+++ app/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>Index</h1>
+</template>
+
+<style lang="scss" scoped>
+h1 {
+    color: $color-red;
+}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,20 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: {
+    enabled: true,
+  },
+  app: {
+    baseURL: import.meta.env.NUXT_APP_BASE_URL || '/',
+  },
+  css: ['~/assets/scss/main.scss'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "~/assets/scss/_variables.scss" as *;',
+        },
+      },
+    },
+  },
+})
